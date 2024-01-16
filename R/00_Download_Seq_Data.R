760d63540889df3f642404d120d84b2fc4568c78
gzahn <gzahn@uvu.edu>
2024-01-15 19:50:54 -0700

    add download script

--- R/00_Download_Seq_Data.R
+++ R/00_Download_Seq_Data.R
@@ -1,16 +1,61 @@
-# download data
 
+#------------------------------------------------------------------#
+# Download raw sequence data from SRA Project PRJNA1064977         #
+#                                                                  #
+# Requires that sra-toolkit be installed and in your system path   #
+# (https://github.com/ncbi/sra-tools)                              #
+#------------------------------------------------------------------#
+
+
+# Setup ####
 library(tidyverse)
 df <- read_csv("./Data/Sample_Metadata_Full.csv")
+fung <- df %>% 
+  dplyr::filter(amplicon == "ITS2")
+bact <- df %>% 
+  dplyr::filter(amplicon == "16S")
 
-accessions <- df$sra_accession
-filenames_f <- file.path("./Data/Raw",df$filename_r1)
-filenames_r <- file.path("./Data/Raw",df$filename_r2)
 
-file.path("./Data/Raw",filenames_f)
+# Download fungal amplicons ####
+accessions <- fung$sra_accession
+filenames_f <- file.path(str_remove(fung$filename_r1,".gz"))
+filenames_r <- file.path(str_remove(fung$filename_r2,".gz"))
+dl_filenames_1 <- paste0(accessions,"_1.fastq")
+dl_filenames_2 <- paste0(accessions,"_2.fastq")
+
 
 # run download in a for-loop for all accessions
+# compress and rename and move files
+if(!dir.exists("./Data/Raw/ITS")){
+  dir.create("./Data/Raw/ITS",recursive = TRUE)
+}
+
 for(i in seq_along(accessions)){
-  x <- paste0("--split-files -o ",filenames_f[i]," ",filenames_r[i]," --skip-technical ",accessions[i])
-  system2("fasterq-dump",args = x)
+  system2("fasterq-dump",args = accessions[i])
+  system2("gzip", args = c(dl_filenames_1[i],dl_filenames_2[i]))
+  file.rename(paste0(dl_filenames_1[i],".gz"),paste0("./Data/Raw/ITS/",fung$filename_r1[i]))
+  file.rename(paste0(dl_filenames_2[i],".gz"),paste0("./Data/Raw/ITS/",fung$filename_r2[i]))
+}
+
+
+
+# Download bacterial amplicons ####
+accessions <- bact$sra_accession
+filenames_f <- file.path(str_remove(bact$filename_r1,".gz"))
+filenames_r <- file.path(str_remove(bact$filename_r2,".gz"))
+dl_filenames_1 <- paste0(accessions,"_1.fastq")
+dl_filenames_2 <- paste0(accessions,"_2.fastq")
+
+
+# run download in a for-loop for all accessions
+# compress and rename and move files
+if(!dir.exists("./Data/Raw/16S")){
+  dir.create("./Data/Raw/16S",recursive = TRUE)
+}
+
+for(i in seq_along(accessions)){
+  system2("fasterq-dump",args = accessions[i])
+  system2("gzip", args = c(dl_filenames_1[i],dl_filenames_2[i]))
+  file.rename(paste0(dl_filenames_1[i],".gz"),paste0("./Data/Raw/16S/",bact$filename_r1[i]))
+  file.rename(paste0(dl_filenames_2[i],".gz"),paste0("./Data/Raw/16S/",bact$filename_r2[i]))
 }
