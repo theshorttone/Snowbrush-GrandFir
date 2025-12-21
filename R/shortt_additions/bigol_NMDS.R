107a63482393c45b67498b7ce12875b9c852d5c8
Zac Shortt <114637164+theshorttone@users.noreply.github.com>
2025-12-20 17:14:36 -0800

    organizing my new figs into a folder, so far just working on NMDS, and other work is scattered and just saved local

---
+++ R/shortt_additions/bigol_NMDS.R
@@ -0,0 +1,202 @@
+library(tidyverse)
+library(phyloseq)
+library(zahntools)
+library(vegan); packageVersion("vegan")
+library(ggnewscale)
+library(ggthemes)
+
+theme_set(theme_minimal())
+source("./R/palettes.R")
+site_colors <- c(
+  "Site 1" = "#4cbfe6", "Site 2" = "#2443f0",
+  "Site 3" = "#f0c424", "Site 4" = "#db7e04",
+  "Site 5" = "#cc6866", "Site 6" = "#9e0402"
+)
+
+# 3 mid-blend colors (averages) for ellipses
+burn_colors <- c(
+  "0" = "#3881F3",
+  "1"   = "#E59D14",
+  "3"   = "#A73634"
+)
+
+
+# plotting functions ------------------------------------------------------
+
+
+make_ord_plot <- function(ps,
+                          method = "NMDS",
+                          distance = "bray",
+                          shape_var = "community",
+                          point_color_var = "plot_nmds_color",
+                          ellipse_color_var = "fire_freq",
+                          ellipse_linetype_var = "community",
+                          site_colors,
+                          burn_colors,
+                          ellipse_level = 0.8,
+                          ellipse_type = "t",
+                          trymax = 20) {
+  
+  
+  # ---- ordination ----
+  ps_rel <- phyloseq::transform_sample_counts(ps, function(x) x / sum(x))
+  
+  ord <- if (toupper(method) == "NMDS") {
+    phyloseq::ordinate(ps_rel, method = "NMDS", distance = distance, trymax = trymax)
+  } else {
+    phyloseq::ordinate(ps_rel, method = method, distance = distance)
+  }
+
+  # ---- base plot (points) ----
+  p <- phyloseq::plot_ordination(
+    ps, ord,
+    shape = shape_var,
+    color = point_color_var,
+    
+  ) +
+    scale_color_manual(values = site_colors,
+                       name = "Inoculation site")+
+    scale_shape_manual(
+      values = c(16, 17),
+      name = "Community",
+    )
+  
+  # ---- second color scale (ellipses) ----
+  p <- p +
+    new_scale_color() +
+    stat_ellipse(
+      aes(
+        color = factor(.data[[ellipse_color_var]]),
+        linetype = .data[[ellipse_linetype_var]],
+        group = interaction(factor(.data[[ellipse_color_var]]), .data[[ellipse_linetype_var]])
+      ),
+      type = ellipse_type,
+      level = ellipse_level,
+      linewidth = 0.7
+    ) +
+    scale_color_manual(values = burn_colors, name = "Fire frequency") +
+    scale_linetype_manual(values = c("solid", "longdash"))+
+    guides(
+      color = guide_legend(order = 1),
+      fill  = guide_legend(order = 2),
+      linetype = "none"
+    )+
+    theme_few()
+  
+  return(list(ord = ord, plot = p))
+}
+
+
+# data cleaning function --------------------------------------------------
+
+
+clean_func <- function (df){
+  df_samples <- df %>%
+    mutate(
+      plot_nmds_color = case_when(
+        !is.na(.data$inoculum) & .data$inoculum != "" ~ str_replace(.data$inoculum, "^.*_", ""),
+        TRUE ~ recode(
+          .data$other_frompreviouscolumn,
+          "Site1" = "Site 1",
+          "Site2" = "Site 2",
+          "Site3" = "Site 3",
+          "Site4" = "Site 4",
+          "Site5" = "Site 5",
+          "Site6" = "Site 6",
+          .default = NA_character_
+        )),
+      plot_nmds_color = recode(plot_nmds_color,
+                               "W1" = "Site 1",
+                               "W2" = "Site 2",
+                               "W3" = "Site 3",
+                               "W4" = "Site 4",
+                               "W5" = "Site 5",
+                               "W6" = "Site 6",
+                               .default = .data$plot_nmds_color
+      ),
+      fire_freq = if_else(
+        .data$other_frompreviouscolumn %in% paste0("Site", 1:6),
+        case_when(
+          .data$other_frompreviouscolumn %in% c("Site1", "Site2") ~ 0,
+          .data$other_frompreviouscolumn %in% c("Site3", "Site4") ~ 1,
+          .data$other_frompreviouscolumn %in% c("Site5", "Site6") ~ 3
+        ),
+        as.numeric(.data$fire_freq)
+      )
+    )
+}
+# 16S ---------------------------------------------------------------------
+
+
+site1.inoc.full <- readRDS("Output/phyloseq_objects/16S_site1.inoc.full.RDS")
+site2.inoc.full <- readRDS("Output/phyloseq_objects/16S_site2.inoc.full.RDS")
+site3.inoc.full <- readRDS("Output/phyloseq_objects/16S_site3.inoc.full.RDS")
+site4.inoc.full <- readRDS("Output/phyloseq_objects/16S_site4.inoc.full.RDS")
+site5.inoc.full <- readRDS("Output/phyloseq_objects/16S_site5.inoc.full.RDS")
+site6.inoc.full <- readRDS("Output/phyloseq_objects/16S_site6.inoc.full.RDS")
+ps <- readRDS("Output/phyloseq_objects/16S_clean_phyloseq_object.RDS")
+
+# merge all inoculum phyloseq objects
+inoc.full <- merge_phyloseq(site1.inoc.full,site2.inoc.full,site3.inoc.full,
+                           site4.inoc.full,site5.inoc.full,site6.inoc.full)
+
+df_samples <- sample_data(inoc.full) 
+df_samples <- data.frame(df_samples)   
+  
+df_samples <- clean_func(df_samples)
+sample_data(inoc.full) <- sample_data(df_samples)
+
+
+make_ord_plot(
+  ps = inoc.full,
+  method = "NMDS",
+  distance = "bray",
+  shape_var = "community",
+  point_color_var = "plot_nmds_color",
+  ellipse_color_var = "fire_freq",
+  ellipse_linetype_var = "community",
+  site_colors = site_colors,
+  burn_colors = burn_colors,
+  ellipse_level = 0.8,
+  ellipse_type = "t",
+  trymax = 20
+)$plot +
+  labs(
+    title = "NMDS of 16S gene sequences",
+    subtitle = " "
+)
+ggsave("R/shortt_additions/figures/bigol_16S_NMDS_inoculum.png",
+       width = 6, height = 5)
+
+
+# Fungal-----------------------------------------------------------------------
+site1.inoc.full <- readRDS("Output/phyloseq_objects/ITS_site1.inoc.full.RDS")
+site2.inoc.full <- readRDS("Output/phyloseq_objects/ITS_site2.inoc.full.RDS")
+site3.inoc.full <- readRDS("Output/phyloseq_objects/ITS_site3.inoc.full.RDS")
+site4.inoc.full <- readRDS("Output/phyloseq_objects/ITS_site4.inoc.full.RDS")
+site5.inoc.full <- readRDS("Output/phyloseq_objects/ITS_site5.inoc.full.RDS")
+site6.inoc.full <- readRDS("Output/phyloseq_objects/ITS_site6.inoc.full.RDS")
+
+inoc.full.fung <- merge_phyloseq(site1.inoc.full,site2.inoc.full,site3.inoc.full,
+                            site4.inoc.full,site5.inoc.full,site6.inoc.full)
+
+df_samples <- sample_data(inoc.full.fung) 
+df_samples <- data.frame(df_samples)   
+
+df_samples <- clean_func(df_samples)
+sample_data(inoc.full.fung) <- sample_data(df_samples)
+
+make_ord_plot(
+  ps = inoc.full.fung,
+  method = "NMDS",
+  distance = "bray",
+  site_colors = site_colors,
+  burn_colors = burn_colors,
+  trymax = 20
+)$plot +
+  labs(
+    title = "NMDS of ITS gene sequences",
+    subtitle = " "
+  )
+ggsave("R/shortt_additions/figures/bigol_ITS_NMDS_inoculum.png",
+       width = 6, height = 5)
