fd2b67a49f10155d4365c188702e5b4fd7294dd4
Zac Shortt <114637164+theshorttone@users.noreply.github.com>
2026-04-03 17:10:01 -0700

    update

--- R/shortt_additions/Step_3_betanti.R
+++ R/shortt_additions/Step_3_betanti.R
@@ -1,5 +1,8 @@
 data.set.name <- "snowbrush_april_2026"
 
+source("R/shortt_additions/assembly_model/raup_crick_abundance_one_comparison.R")
+source("R/shortt_additions/assembly_model/beta.nti.R")
+
 out_dir <- "./outputs/assembly_model"
 null_dir <- file.path(out_dir, "null_reps")
 
