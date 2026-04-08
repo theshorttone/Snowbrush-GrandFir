8e686fc5dc4f923f165e8e609ade52d4e0512f2c
Zac Shortt <114637164+theshorttone@users.noreply.github.com>
2026-04-08 10:06:57 -0700

    fixing paths

--- R/shortt_additions/Step_3_betanti.R
+++ R/shortt_additions/Step_3_betanti.R
@@ -2,16 +2,21 @@ data.set.name <- "snowbrush_april_2026"
 
 source("R/shortt_additions/assembly_model/beta.nti.R")
 
-out_dir <- "./outputs/assembly_model"
+out_dir  <- "./outputs/assembly_model"
 null_dir <- file.path(out_dir, "null_reps")
 
+input_rds <- file.path(out_dir, paste0(data.set.name, "_OTU_table.RDS"))
+otu <- readRDS(input_rds)
+
+# observed bMNTD matrix
 beta.mntd.weighted <- read.csv(
   file.path(out_dir, paste0(data.set.name, "_bMNTD_weighted.csv")),
-  row.names = 1
+  row.names = 1,
+  check.names = FALSE
 )
 
-#get all null model files
-all.files <- sort(as.numeric(
+# get all null model rep numbers
+all.files <- sort(as.integer(
   gsub(
     ".*_rep_|\\.csv", "",
     list.files(
